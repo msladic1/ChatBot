6209ef7743a8a49f350825947990ef18c4157e8a
msladic1 <sladic.muris@gmail.com>
2023-06-20 13:53:33 +0200

    Implementing personality feature

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,6 @@
 import openai
 from dotenv import dotenv_values
+import argparse
 
 config = dotenv_values(".env")
 openai.api_key = config["OPENAI_API_KEY"]
@@ -9,20 +10,30 @@ res = openai.ChatCompletion.create(
     messages = [{"role": "user", "content": "What is the best car in the world? "}]
 )
 
-messages = []
-while True:
-    try:
-        user_input = input("\nYou: ")
-        messages.append({"role": "user", "content": user_input})
-        res = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages = messages
-        )
-        messages.append(res["choices"][0]["message"].to_dict())
-        print("\nAssistant: ", res["choices"][0]["message"]["content"])
+def main():
+    parser = argparse.ArgumentParser(description = "Simple command line with GPT-3.5-turbo")
+    parser.add_argument("--personality", type=str, help="A brief summary of chatbot's personality", default="friendly and helpful chatbot")
 
-    except KeyboardInterrupt:
-        print("Exiting...")
-        break
+    args = parser.parse_args()
 
-print(res)
+    initial_prompt = f"You are a conversational chatbot. Your personality is: {args.personality}"
+    messages = [{"role": "system", "content": initial_prompt}]
+    while True:
+        try:
+            user_input = input("\nYou: ")
+            messages.append({"role": "user", "content": user_input})
+            res = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages = messages
+            )
+            messages.append(res["choices"][0]["message"].to_dict())
+            print("\nAssistant: ", res["choices"][0]["message"]["content"])
+
+        except KeyboardInterrupt:
+            print("Exiting...")
+            break
+
+    print(res)
+
+if __name__ == "__main__":
+    main()
